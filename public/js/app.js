91272cf3e83bf9334e9dd20c9bfba8082d7d3939
James Abney <james.abney@gmail.com>
2017-10-02 19:06:19 -0700

    Add app.js

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+(function (window, namespace) {
+  'use strict';
+  var _project = window[namespace] = Object.create(null);
+
+  _project.scrollListener = function scrollListener(throttleTime, callback) {
+    var eventStack = []
+
+    function onScroll() {
+      if (eventStack.length === 0) {
+        eventStack.push(callback)
+        window.setTimeout(function () {
+          if (eventStack.length) {
+            eventStack.pop()()
+          }
+          while(eventStack.length) {
+            eventStack.pop()
+          }
+        }, throttleTime);
+      }
+
+      else {
+        eventStack.push(callback)
+      }
+    }
+
+    window.addEventListener('scroll', onScroll)
+
+    return function () {
+      window.removeEventListener('scroll', onScroll)
+    }
+
+  };
+
+})(window, 'rbproject')
