00402efcfacbbb27004c47a030280e745d8e0ede
James Abney <james.abney@gmail.com>
2017-10-03 22:11:43 -0700

    Add to project

---
+++ public/js/scrolling.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+
+  $(window).on('load resize', function () {
+    $('.fill-screen').css('height', window.innerHeight)
+  })
+
+  // Document ready
+  $(function () {
+
+  })
+})()
