2ab5ba623e7b814fa4e06443437c1517a05857c0
James Abney <james.abney@gmail.com>
2017-10-05 12:16:53 -0700

    Configure scrollspy and smooth scrolling

--- public/js/scrolling.js
+++ public/js/scrolling.js
@@ -7,6 +7,15 @@
 
   // Document ready
   $(function () {
-
+    $('body').scrollspy({
+      target: '.navbar',
+      offset: 160
+    })
+    $('nav a, .down-button a').bind('click', function (e) {
+      $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top - 60
+      }, 1000, 'easeInOutQuart')
+      e.preventDefault()
+    })
   })
 })()
