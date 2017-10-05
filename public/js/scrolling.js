8da576925385623a7850e042b490491561c932a1
James Abney <james.abney@gmail.com>
2017-10-05 13:02:23 -0700

    Configure stellar.js

--- public/js/scrolling.js
+++ public/js/scrolling.js
@@ -7,15 +7,19 @@
 
   // Document ready
   $(function () {
+
     $('body').scrollspy({
       target: '.navbar',
       offset: 160
     })
+
     $('nav a, .down-button a').bind('click', function (e) {
       $('html, body').stop().animate({
         scrollTop: $($(this).attr('href')).offset().top - 60
       }, 1000, 'easeInOutQuart')
       e.preventDefault()
     })
+
+    $(window).stellar()
   })
 })()
